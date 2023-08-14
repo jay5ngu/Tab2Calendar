69162f8c693ff3ec475bb6a5f5c241fa5e3b0865
Jayson Nguyen <jaysonn@uci.edu>
2023-08-14 12:48:43 -0700

    fixed url retrieval; now gives consistent domain names from url

--- tabsChromeExtension/background.js
+++ tabsChromeExtension/background.js
@@ -6,17 +6,40 @@ var previousUrl = null;
 chrome.tabs.onActivated.addListener(function (activeInfo)
 {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
-        var newUrl = new URL(url);
-        console.log("Printing from onActivated(): " + newUrl.hostname);
+        if (tabs[0].status == "complete")
+        {
+            var newUrl = tabs[0].url;
+            newUrl = new URL(newUrl);
+            newUrl = newUrl.hostname.replace("www.", "");
+            console.log("Printing from onActivated(): " + newUrl);
+            if (previousUrl == null)
+            {
+                previousUrl = newUrl;
+            }
+            else if (previousUrl != newUrl)
+            {
+                previousUrl = newUrl;
+                console.log("URL changed to " + previousUrl);
+            }
+        }
     });
 });
 
 // for when you go to new url from current tab
 chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-    if (tab.status == "complete" && tab.url != 'chrome://newtab/')
+    if (tab.status == "complete") // && tab.url != 'chrome://newtab/'
     {
         var newUrl = new URL(tab.url);
-        console.log("Printing from onUpdated() " + newUrl.hostname);
+        newUrl = newUrl.hostname.replace("www.", "");
+        console.log("Printing from onUpdated() " + newUrl);
+        if (previousUrl == null)
+        {
+            previousUrl = newUrl;
+        }
+        else if (previousUrl != newUrl)
+        {
+            previousUrl = newUrl;
+            console.log("URL changed to " + previousUrl);
+        }
     }
 });
